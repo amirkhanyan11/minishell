c5bbb2ab248191c0fbd3b2990e741ffe997e0a17
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-07-10 20:17:58 +0400

    .

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -6,12 +6,13 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 15:50:58 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/07/09 21:39:25 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/07/10 20:17:54 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// change the function so that it takest the raw string or something and prints out with the whitespace characters
 void echo(t_command *cmd)
 {
 	if (!cmd) __exit("nullptr passed to echo");
