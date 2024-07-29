aecbede115f7d36ea4974c48bd0536dc36886f8e
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-07-29 19:41:12 +0400

    .

--- src/exec/set_descriptors.c
+++ src/exec/set_descriptors.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 22:32:22 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/07/29 17:16:29 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/07/29 19:37:09 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,7 @@ void set_descriptors(t_command * cmd)
 {
 	if (!cmd) return ;
 
-	if (cmd->descriptors->stdin != cmd->shell->stddesc->stdin)
-		dup2(cmd->descriptors->stdin, STDIN_FILENO);
-	if (cmd->descriptors->stdout != cmd->shell->stddesc->stdout)
-		dup2(cmd->descriptors->stdout, STDOUT_FILENO);
-	if (cmd->descriptors->stderr != cmd->shell->stddesc->stderr)
-		dup2(cmd->descriptors->stderr, STDERR_FILENO);
+	dup2(cmd->descriptors->stdin, STDIN_FILENO);
+	dup2(cmd->descriptors->stdout, STDOUT_FILENO);
+	dup2(cmd->descriptors->stderr, STDERR_FILENO);
 }
