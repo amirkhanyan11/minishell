149f804d06780a3f73107901f0765c33b48c49bc
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-08-01 23:06:09 +0400

    .

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -6,15 +6,15 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 19:30:39 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/08/01 23:00:52 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/08/01 23:05:56 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void update_pwd(t_shell *shell, char *oldpwd);
-static void cd_one_arg(t_command *cmd);
-static void cd_no_arg(t_command *cmd);
+static void __cd_one_arg__(t_command *cmd);
+static void __cd_no_arg__(t_command *cmd);
 static void _chdir(const char *path);
 
 void cd(t_command *cmd)
@@ -31,9 +31,9 @@ void cd(t_command *cmd)
 
 	string oldpwd = get_value(cmd->shell->export, "PWD");
 
-	if (empty(cmd->args)) cd_no_arg(cmd);
+	if (empty(cmd->args)) __cd_no_arg__(cmd);
 
-	else cd_one_arg(cmd);
+	else __cd_one_arg__(cmd);
 
 	update_pwd(cmd->shell, oldpwd);
 }
@@ -44,14 +44,14 @@ static void _chdir(const char *path)
 		__perror(strerror(errno));
 }
 
-static void cd_no_arg(t_command *cmd)
+static void __cd_no_arg__(t_command *cmd)
 {
 	string home = get_value(cmd->shell->export, "HOME");
 	_chdir(home);
 
 }
 
-static void cd_one_arg(t_command *cmd)
+static void __cd_one_arg__(t_command *cmd)
 {
 	if (find_range(cmd->args, "-", NULL) != NULL)
 	{
@@ -68,7 +68,7 @@ static void cd_one_arg(t_command *cmd)
 	}
 	else if (find_range(cmd->args, "~", NULL))
 	{
-		cd_no_arg(cmd);
+		__cd_no_arg__(cmd);
 	}
 	else
 		_chdir(cmd->args->head->val);
