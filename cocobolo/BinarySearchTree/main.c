8ddb2388dc61d68e3196e3e9484afcaf1bf32766
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-08-01 18:36:13 +0400

    .

--- cocobolo/BinarySearchTree/main.c
+++ cocobolo/BinarySearchTree/main.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marikhac <marikhac@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 17:28:29 by marikhac          #+#    #+#             */
-/*   Updated: 2024/08/01 18:12:19 by marikhac         ###   ########.fr       */
+/*   Updated: 2024/08/01 18:36:09 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 
-int main()
-{
-	int arr[] = {8, 1, 2, 9, 6, 55, 4, 7, 5, 3};
-	const int size = sizeof(arr) / sizeof(int);
+// int main()
+// {
+// 	int arr[] = {8, 1, 2, 9, 6, 55, 4, 7, 5, 3};
+// 	const int size = sizeof(arr) / sizeof(int);
 
-	t_tree *tree = make_tree_from_array(arr, size);
-	print_preorder(tree);
-	node_clear(tree, 8);
-	print_preorder(tree);
-	node_clear(tree, 7);
-	print_preorder(tree);
-	return 0;
-}
+// 	t_tree *tree = make_tree_from_array(arr, size);
+// 	print_preorder(tree);
+// 	node_clear(tree, 8);
+// 	print_preorder(tree);
+// 	node_clear(tree, 7);
+// 	print_preorder(tree);
+// 	return 0;
+// }
