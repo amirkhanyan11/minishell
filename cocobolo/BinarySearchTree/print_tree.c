1280605bcde56cb486a50402dff1064293b6c8a0
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-08-01 22:28:41 +0400

    .

--- cocobolo/BinarySearchTree/print_tree.c
+++ cocobolo/BinarySearchTree/print_tree.c
@@ -6,19 +6,19 @@ static void __print_val__(tree_node *root)
 	printf("%d ", root->val);
 }
 
-void print_preorder(t_tree *tree)
+void print_tree_preorder(t_tree *tree)
 {
 	preorder(tree, __print_val__);
 	printf("\n");
 }
 
-void print_inorder(t_tree *tree)
+void print_tree_inorder(t_tree *tree)
 {
 	inorder(tree, __print_val__);
 	printf("\n");
 }
 
-void print_postorder(t_tree *tree)
+void print_tree_postorder(t_tree *tree)
 {
 	postorder(tree, __print_val__);
 	printf("\n");
--- src/lifecycle/make_cmd_container.c
+++ src/lifecycle/make_cmd_container.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 17:35:09 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/07/29 18:57:28 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/08/01 20:22:53 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ t_cmd_container * __result_use_check make_cmd_container(char * raw_cmd)
     a_list tokens = preprocess(tokenize(raw_cmd));
 
 	if (empty(tokens)) return NULL;
-    
+
     t_cmd_container * cmds = __malloc(sizeof(t_cmd_container));
 
     cmds->size = count_range(tokens, "|") + 1;
@@ -25,12 +25,12 @@ t_cmd_container * __result_use_check make_cmd_container(char * raw_cmd)
     cmds->arr = __malloc(sizeof(t_command) * cmds->size);
 
     size_t i = 0;
-    
+
     t_node *first = tokens->head;
     while (i < cmds->size)
     {
         t_node *pipe = find(first, tokens->tail, "|", NULL);
-        
+
         if (!pipe) pipe = tokens->tail;
 
         else
@@ -38,7 +38,7 @@ t_cmd_container * __result_use_check make_cmd_container(char * raw_cmd)
             pipe = pipe->prev;
             pop(tokens, pipe->next);
         }
-        
+
         a_list partition = make_list_copy(first, pipe, NULL);
 
         cmds->arr[i] = make_command(partition);
