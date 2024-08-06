ffc68f5ecea19e4951a0d5424215fdda76653425
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-08-06 19:12:46 +0400

    .

--- src/exec/eval.c
+++ src/exec/eval.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 15:29:45 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/08/06 18:51:58 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/08/06 19:12:20 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,18 @@ void eval(t_cmd_container *cmds)
 	{
 		t_command *cmd = cmds->arr[cmds->current_cmd_index];
 
-		if (cmd) cmd->eval(cmd);
-
-		export_update(cmd->shell, "_", cmd->name);
-
-		if (cmd->redirection & redirect_heredoc)
+		if (cmd)
 		{
-			unlink(heredoc);
-			printf("\n"); // not here but in the end of all commands :)
-		}
+			cmd->eval(cmd);
 
+			export_update(cmd->shell, "_", cmd->name);
+
+			if (cmd->redirection & redirect_heredoc)
+			{
+				unlink(heredoc);
+				printf("\n"); // not here but in the end of all commands :)
+			}
+		}
 		cmds->current_cmd_index++;
 	}
 	while (-1 != wait(NULL));
