44f78aed9a49c73bc7d00e2033ab9e7bd78720e6
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-08-18 22:09:38 +0400

    .

--- cocobolo/tree/rotate.c
+++ cocobolo/tree/rotate.c
@@ -6,7 +6,7 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/09 20:17:58 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/08/18 21:59:10 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/08/18 22:02:41 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,38 @@
 
 void	left_rotate(t_tree *tree, tree_node *x)
 {
-	tree_node *y = x->right;
+	tree_node	*y;
 
+	y = x->right;
 	x->right = y->left;
-
-	if (y->left != tree->NIL) y->left->p = x;
-
+	if (y->left != tree->NIL)
+		y->left->p = x;
 	y->p = x->p;
-
-    if (x->p == tree->NIL) tree->root = y;
-
-    else if (x == x->p->left) x->p->left = y;
-
-    else x->p->right = y;
-
-    y->left = x;
-
-    x->p = y;
+	if (x->p == tree->NIL)
+		tree->root = y;
+	else if (x == x->p->left)
+		x->p->left = y;
+	else
+		x->p->right = y;
+	y->left = x;
+	x->p = y;
 }
 
 void	right_rotate(t_tree *tree, tree_node *x)
 {
-	tree_node *y = x->left;
+	tree_node	*y;
 
+	y = x->left;
 	x->left = y->right;
-
-	if (y->right != tree->NIL) y->right->p = x;
-
+	if (y->right != tree->NIL)
+		y->right->p = x;
 	y->p = x->p;
-
-    if (x->p == tree->NIL) tree->root = y;
-
-    else if (x == x->p->right) x->p->right = y;
-
-    else x->p->left = y;
-
-    y->right = x;
-
-    x->p = y;
+	if (x->p == tree->NIL)
+		tree->root = y;
+	else if (x == x->p->right)
+		x->p->right = y;
+	else
+		x->p->left = y;
+	y->right = x;
+	x->p = y;
 }
