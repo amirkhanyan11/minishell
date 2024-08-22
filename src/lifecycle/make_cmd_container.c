021c38e36f4eafcad01f7baa07f5cf4703edeae7
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-08-22 19:12:59 +0400

    .

--- src/lifecycle/make_cmd_container.c
+++ src/lifecycle/make_cmd_container.c
@@ -6,72 +6,71 @@
 /*   By: aamirkha <aamirkha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 17:35:09 by aamirkha          #+#    #+#             */
-/*   Updated: 2024/08/22 19:11:32 by aamirkha         ###   ########.fr       */
+/*   Updated: 2024/08/22 19:12:43 by aamirkha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_list *make_partition(t_shell *shell, t_node *first, t_node *last);
-static void make_cmds(t_cmd_container *container, t_shell *shell, t_list *tokens);
+static t_list	*make_partition(t_shell *shell, t_node *first, t_node *last);
+static void		make_cmds(t_cmd_container *container, t_shell *shell,
+					t_list *tokens);
 
-t_cmd_container *make_cmd_container(char * raw_cmd, t_shell *shell)
+t_cmd_container	*make_cmd_container(char *raw_cmd, t_shell *shell)
 {
-    scoped_list tokens = preprocess(tokenize(raw_cmd), shell);
+	scoped_list		tokens;
+	t_cmd_container	*container;
 
-	if (empty(tokens) || !shell) return NULL;
-
-    t_cmd_container * container = __malloc(sizeof(t_cmd_container));
-
+	tokens = preprocess(tokenize(raw_cmd), shell);
+	if (empty(tokens) || !shell)
+		return (NULL);
+	container = __malloc(sizeof(t_cmd_container));
 	container->current_cmd_index = 0;
-
 	container->shell = shell;
-
 	container->size = count_range(tokens, "|") + 1;
-
-    container->arr = __malloc(sizeof(t_command) * container->size);
-
+	container->arr = __malloc(sizeof(t_command) * container->size);
 	container->shell->container = container;
-
 	make_cmds(container, shell, tokens);
-
-    return container;
+	return (container);
 }
 
-
-static void make_cmds(t_cmd_container *container, t_shell *shell, t_list *tokens)
+static void	make_cmds(t_cmd_container *container, t_shell *shell,
+		t_list *tokens)
 {
-    size_t i = 0;
+	size_t	i;
+	t_node	*first;
+	t_node	*pipe;
+	t_list	*partition;
 
-    t_node *first = front(tokens);
-    while (i < container->size)
-    {
-        t_node *pipe = find(first, back(tokens), "|", NULL);
-
-        if (!pipe) pipe = back(tokens);
-
-        else
-        {
-            pipe = pipe->prev;
-            pop(tokens, pipe->next);
-        }
-
-        scoped_list partition = make_partition(shell, first, pipe);
-
-        container->arr[i] = make_command(partition, container, shell);
-
-        first = pipe->next;
-        i++;
-    }
+	i = 0;
+	first = front(tokens);
+	while (i < container->size)
+	{
+		pipe = find(first, back(tokens), "|", NULL);
+		if (!pipe)
+			pipe = back(tokens);
+		else
+		{
+			pipe = pipe->prev;
+			pop(tokens, pipe->next);
+		}
+		partition = make_partition(shell, first, pipe);
+		container->arr[i] = make_command(partition, container, shell);
+		list_clear(&partition);
+		first = pipe->next;
+		i++;
+	}
 }
 
-static t_list *make_partition(t_shell *shell, t_node *first, t_node *last)
+static t_list	*make_partition(t_shell *shell, t_node *first, t_node *last)
 {
-	if (!shell || !first || !last) return NULL;
+	t_list	*list;
+	t_node	*curr;
 
-	t_list *list = make_list();
-	t_node *curr = first;
-
+	if (!shell || !first || !last)
+		return (NULL);
+	list = make_list();
+	curr = first;
 	while (curr && curr->prev != last)
 	{
 		push_back(list, curr->val);
@@ -81,5 +80,5 @@ static t_list *make_partition(t_shell *shell, t_node *first, t_node *last)
 		}
 		curr = curr->next;
 	}
-	return list;
+	return (list);
 }
